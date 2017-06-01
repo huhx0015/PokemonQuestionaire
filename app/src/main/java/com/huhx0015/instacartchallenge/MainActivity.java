8713465d52ecbbfd5befd7aa17abdfa5f1607ba8
huhx0015 <mhuh@yoonhuh.com>
2017-05-31 17:39:15 -0700

    content_main.xml: Added RelativeLayout to serve as fragment container.

--- app/src/main/java/com/huhx0015/instacartchallenge/MainActivity.java
+++ app/src/main/java/com/huhx0015/instacartchallenge/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/huhx0015/instacartchallenge/Questions.java
+++ app/src/main/java/com/huhx0015/instacartchallenge/Questions.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class QuestionsResponse {
+public class Questions {
 
     @SerializedName("365 Organic Frosted Flakes Cereal")
     @Expose
