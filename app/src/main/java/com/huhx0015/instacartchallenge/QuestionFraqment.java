4849e955850d6e300e8bb35c057395621fdf48fb
huhx0015 <mhuh@yoonhuh.com>
2017-05-31 17:43:17 -0700

    Added QuestionFragment and ResultFragment fragment classes for displaying the questions and the result.

---
+++ app/src/main/java/com/huhx0015/instacartchallenge/QuestionFraqment.java
@@ -0,0 +1,22 @@
+package com.huhx0015.instacartchallenge;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Michael Yoon Huh on 5/31/2017.
+ */
+
+public class QuestionFraqment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_question, container, false);
+        return view;
+    }
+}
