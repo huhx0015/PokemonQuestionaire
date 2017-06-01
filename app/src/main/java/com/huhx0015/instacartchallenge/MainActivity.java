0db502e2e646bcd22a859d3c88d5616ec6b2b1bb
huhx0015 <mhuh@yoonhuh.com>
2017-05-31 17:49:32 -0700

    MainActivity: Added initView() and loadFragment() methods.

--- app/src/main/java/com/huhx0015/instacartchallenge/MainActivity.java
+++ app/src/main/java/com/huhx0015/instacartchallenge/MainActivity.java
@@ -1,17 +1,34 @@
 package com.huhx0015.instacartchallenge;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String mFragmentTag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
+    private void loadFragment(Fragment fragment, String tag) {
+        this.mFragmentTag = tag;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_fragment_container, fragment);
+        fragmentTransaction.commitAllowingStateLoss();
     }
 }
