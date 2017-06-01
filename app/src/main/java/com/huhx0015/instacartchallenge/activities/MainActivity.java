4bcf1249ed7f8b2e72f1860c2f0eed12ccd33e96
huhx0015 <mhuh@yoonhuh.com>
2017-05-31 18:52:57 -0700

    MainActivity: Added AsyncTask for loading JSON file from assets.

--- app/src/main/java/com/huhx0015/instacartchallenge/activities/MainActivity.java
+++ app/src/main/java/com/huhx0015/instacartchallenge/activities/MainActivity.java
@@ -1,23 +1,34 @@
 package com.huhx0015.instacartchallenge.activities;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.huhx0015.instacartchallenge.constants.GroceryConstants;
 import com.huhx0015.instacartchallenge.fragments.QuestionFraqment;
 import com.huhx0015.instacartchallenge.R;
+import com.huhx0015.instacartchallenge.utils.JsonUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String mFragmentTag;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    @BindView(R.id.main_fragment_container) FrameLayout mFragmentContainer;
+    private String mFragmentTag;
+    private String mJsonString;
+
+    @BindView(R.id.main_fragment_container) RelativeLayout mFragmentContainer;
     @BindView(R.id.main_progress_bar) ProgressBar mProgressBar;
 
     @Override
@@ -28,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
         initView();
 
+        loadJson();
+
         // TODO: Load first fragment now, change later.
         loadFragment(new QuestionFraqment(), QuestionFraqment.class.getSimpleName());
     }
@@ -37,11 +50,43 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    private void loadJson() {
+        JsonAsyncTask task = new JsonAsyncTask();
+        task.execute(GroceryConstants.GROCERY_ASSET_NAME);
+    }
+
     private void loadFragment(Fragment fragment, String tag) {
         this.mFragmentTag = tag;
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(mFragmentContainer.getId(), fragment);
         fragmentTransaction.commitAllowingStateLoss();
+    }
+
+    public class JsonAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            return JsonUtils.loadJsonFromAsset(GroceryConstants.GROCERY_ASSET_NAME, MainActivity.this);
+        }
+
+        @Override
+        protected void onPostExecute(String string) {
+            super.onPostExecute(string);
+            mProgressBar.setVisibility(View.GONE);
+
+            if (string == null) {
+                Toast.makeText(MainActivity.this, "An error occurred while attempting to read the JSON file.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Log.d(LOG_TAG, string);
+            }
+        }
     }
 }
